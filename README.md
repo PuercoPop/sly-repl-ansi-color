fc568f317a8a8742aafacf25927708da85b063ca
Javier Olaechea <pirata@gmail.com>
2015-04-25 15:52:35 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,18 @@
+SLY-REPL-ANSI-COLOR
+==============
+
+Add ANSI colors support to the sly mrepl. It is a port from the slime version of Max Mikhanosha.
+
+Installation
+------------
+
+Place **sly-repl-ansi-color.el** in your emacs-path and enable to contrib by default by writing the following in your **init.el**
+
+```elisp
+(push 'sly-repl-ansi-color sly-contribs)
+```
+
+License
+-------
+
+The code is licensed under the GPL. See [LICENSE][LICENSE] for details.
---
+++ sly-repl-ansi-color.el
@@ -0,0 +1,40 @@
+;; Ported from is Max Mikhanosha's code.
+(require 'ansi-color)
+
+(define-sly-contrib sly-repl-ansi-color
+  "Turn on ANSI colors in the mREPL output"
+  (:authors '("Javier Olaechea" "Max Mikhanosha"))
+  (:license "GPL")
+  (:sly-dependencies sly-mrepl)
+  (:on-load (progn
+              (sly-repl-ansi-on)
+              (add-hook 'sly-mrepl-output-filter-functions
+                        'sly-repl-ansi-colorize))))
+
+(defvar sly-repl-ansi-color t
+  "When Non-NIL will process ANSI colors in the lisp output")
+
+(make-variable-buffer-local 'sly-repl-ansi-color)
+
+(defun sly-repl-ansi-on ()
+  "Set `ansi-color-for-comint-mode' to t."
+  (interactive)
+  (setq sly-repl-ansi-color t))
+
+(defun sly-repl-ansi-off ()
+  "Set `ansi-color-for-comint-mode' to t."
+  (interactive)
+  (setq sly-repl-ansi-color nil))
+
+(defun sly-repl-ansi-colorize (string)
+  (when sly-repl-ansi-color
+    (with-temp-buffer
+      (insert (ansi-color-apply string))
+      (dotimes (char-pos (- (point-max) (point-min)))
+        (let* ((char-pos (1+ char-pos))
+               (prop (getf (text-properties-at char-pos) 'font-lock-face)))
+          (when prop
+            (put-text-property char-pos (1+ char-pos) 'face prop))))
+      (buffer-string))))
+
+(provide 'sly-repl-ansi-color)
